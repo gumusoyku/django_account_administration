632d2376af0db977e0f00f07840c6689adef6aa0
Oyku Gumus <gumussoyku@gmail.com>
2018-03-16 17:44:37 +0300

    separate settings module and add gitignore for the root directory

--- account_administration/account_administration/settings/base.py
+++ account_administration/account_administration/settings/base.py
@@ -22,12 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mjyeukont*()8avbu2i_l-oa#mg&om@yebumhm+f0)80#if!s4'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -68,18 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'account_administration.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
---
+++ account_administration/account_administration/settings/test.py
@@ -0,0 +1,21 @@
+"""
+Django's settings module has been separated into 3 pieces for each development environment to be convenient.
+This file contains settings for test environments.
+"""
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
