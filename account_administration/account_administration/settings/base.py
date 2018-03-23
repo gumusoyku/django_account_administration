9cf5f3065c42040e892e73bd00fad7e15a1ec1c6
Oyku Gumus <gumussoyku@gmail.com>
2018-03-23 20:07:08 +0300

    create social_auth app for social authentication and add custom pipeline

--- account_administration/account_administration/settings/base.py
+++ account_administration/account_administration/settings/base.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'social_django',
+
+    'social_auth',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +83,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
+    'social_auth.pipeline.admin_auth.set_staff_status_true',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
     'social_core.pipeline.debug.debug',
